4370a1f71450490772cd9bff3c5920fec16d7d86
KSB8 <aqaq925120k@gmail.com>
2017-11-18 14:41:12 +0900

    first assignment

---
+++ [CP1]HW01/CP1_HW1_3/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lineNumber;
+		
+		lineNumber = 1;
+		System.out.println(lineNumber + ". 내 안에 이렇게 눈이 부시게 고운 꽃이 있었다는 것을 나도 몰랐습니다. ");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". 몰랐어요.");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". ");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". 정말 몰랐습니다.");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". 처음이에요 당신에게 나는");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". 이 세상 처음으로");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". 한 송이 꽃입니다.");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". ");
+		lineNumber = lineNumber + 1;
+		System.out.println(lineNumber + ". -김용택, 당신의 꽃-");
+	}
+
+}
